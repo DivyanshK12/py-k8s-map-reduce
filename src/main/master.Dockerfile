32e27e8e05661411d123f8cd4e82c0dc4c0e9d10
Divyansh Kharbanda <divyanshkharbanda@gmail.com>
2024-02-18 20:08:27 +0530

    Added Cassandra , removed volume storage

--- src/main/master.Dockerfile
+++ src/main/master.Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.11
 RUN python3 --version
 
 RUN pip install kafka-python
+RUN pip install cassandra-driver
 
 COPY master.py /
 
--- src/main/master.py
+++ src/main/master.py
@@ -1,13 +1,22 @@
 import os
 from kafka import KafkaProducer, KafkaConsumer
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
 def main():
     num_mappers, num_reducers = os.getenv("MAP_POD_COUNT", 3), os.getenv("REDUCE_POD_COUNT", 2)
     print(f"Master Running with config mappers : {num_mappers} reducers : {num_reducers}")
-    
+
+def test_cassandra():
+    auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
+    cluster = Cluster(['cassandra'], auth_provider=auth_provider)
+    session = cluster.connect()
+    print(session.get_pool_state())
+
+def test_kafka() :
     producer = KafkaProducer(bootstrap_servers=['kafka-core:9092']) # TODO : Read this from env
-    # producer.send('my-topic', key=b'foo2', value=b'bar2')
-    # producer.flush()
+    producer.send('my-topic', key=b'foo2', value=b'bar2')
+    producer.flush()
 
     print("Messages sent")
 
@@ -19,4 +28,4 @@ def main():
                                           message.value.decode('utf-8')))
 
 if __name__ == "__main__":
-    main()
+    test_cassandra()
